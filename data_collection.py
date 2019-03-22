b97ad511b0efac4f007611e9aaa354986bccec20
kazami <kazamizero@gmail.com>
2019-03-22 16:32:37 +0800

    modify the content read write interface for data collection

--- data_collection.py
+++ data_collection.py
@@ -4,8 +4,8 @@ import os
 from netCDF4 import Dataset
 
 class DataCollection:
-    def __init__(self, data):
-        self._data = None
+    def __init__(self, data=None):
+        self._data = data
     def collect(self):
         return
 
@@ -16,15 +16,15 @@ class DataCollection:
         raise NotImplementedError("not implemeted")
 
 class AirDynamicData(DataCollection):
-    def __init__(self, data):
+    def __init__(self, name):
+        data = Dataset("%s.nc"%name, "r")
         super().__init__(data)
         
     def air_collect(self, name, time):
         timezone = self._data["time"][:]
         for i in range(0, len(timezone)):
             if timezone[i] == time:
-                return 
-        return 
+                return self._data[name][i].filled()
 
     def collect(self, name, time):
         return self.air_collect(name, time)
@@ -38,22 +38,4 @@ class AirDynamicData(DataCollection):
         elif "%s.nc"%name in os.listdir(os.getcwd()):
             f = Dataset("%s.nc"%name, "a")
     
-
-
-
-def Input(name, t):
-	time =  Dataset("%s.nc"%name, "r")["time"][:]
-	for i in range(0,len(time)):
-		if t == time[i]:
-			Var = Dataset("%s.nc"%name, "r+")[name]
-	return Var
-
-def Output(name, var, t):
-    global nX, nY
-    if "%s.nc"%name not in os.listdir(os.getcwd()):
-        f = Dataset("%s.nc"%name, "w")
-    elif "%s.nc"%name in os.listdir(os.getcwd()):
-        f = Dataset("%s.nc"%name, "a")
-    return f	
-
    
